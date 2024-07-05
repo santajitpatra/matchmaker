e9eaa71d42264da7688812460a63b4d635f20704
Santajit Patra <business.santajitpatra@gmail.com>
2024-07-06 02:52:56 +0530

    feat(auth): Secure Home page with Kinde authentication
    feat(auth): Secure Home page with Kinde authentication
    
    * Leverages Kinde to secure access to the Home page.
    * Employs `getKindeServerSession` for authentication and user information retrieval.
    * Redirects unauthenticated users to a login page with a post-login redirect.
    * Ensures only authenticated users can access the Home page content.
    * Displays the user's name on the Home page after successful authentication.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = async() => {
+  const {isAuthenticated, getUser} = getKindeServerSession();
+
+  if (!(await isAuthenticated())) {
+    return redirect("/api/auth/login?post_login_redirect_url=http://localhost:3000/callback");
+  }
+
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login?post_login_redirect_url=http://localhost:3000/callback");
+  }
+
+  return <main>{user.given_name}</main>;
 };
 
 export default Home;
